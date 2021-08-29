4ef0992b3ba7856e83f736642cc8e801c6edec4e
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-29 20:39:44 +0530

    Visualizer updated

--- visualizer.py
+++ visualizer.py
@@ -3,7 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # Visualization function for NX graph or PyTorch tensor
-def visualize(G, h, color, epoch=None, loss=None):
+def visualize(h, color, epoch=None, loss=None):
 
     plt.figure(figsize=(7,7))
     plt.xticks([])
@@ -14,7 +14,5 @@ def visualize(G, h, color, epoch=None, loss=None):
         plt.scatter(h[:, 0], h[:, 1], s=140, c=color, cmap="Set2")
         if epoch is not None and loss is not None:
             plt.xlabel(f'Epoch: {epoch}, Loss: {loss.item():.4f}', fontsize=16)
-    else:
-        nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
-                         node_color=color, cmap="Set2")
+
     plt.show()
