ca168534238722f2ef2f534023f40062ef2f8ead
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-29 21:01:01 +0530

    Visualizer updated

--- visualizer.py
+++ visualizer.py
@@ -10,9 +10,13 @@ def visualize(h, color, epoch=None, loss=None):
     plt.yticks([])
 
     if torch.is_tensor(h):
+        print("yo")
         h = h.detach().cpu().numpy()
         plt.scatter(h[:, 0], h[:, 1], s=140, c=color, cmap="Set2")
         if epoch is not None and loss is not None:
             plt.xlabel(f'Epoch: {epoch}, Loss: {loss.item():.4f}', fontsize=16)
+    else:
+        nx.draw_networkx(h, pos=nx.spring_layout(h, seed=42), with_labels=False,
+                         node_color=color, cmap="Set2")
 
     plt.show()
